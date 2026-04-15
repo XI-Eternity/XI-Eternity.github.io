9ceec307fc414c064b87adf6c12fe46058d109ef
XI-Eternity <1109537304@qq.com>
2026-04-15 16:59:28 +0800

    Site updated: 2026-04-15 16:59:28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776243141028";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1776243565730";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
